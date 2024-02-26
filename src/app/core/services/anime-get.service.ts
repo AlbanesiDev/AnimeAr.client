ff8bdd67ff0928a8f3a398f54d7b680384f851ae
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-02-26 18:06:25 -0300

    feat: Service for get anime operations.

---
+++ src/app/core/services/anime-get.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Anime } from "../models/anime.interface";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AnimeGetService {
+  apiBaseUrl = "https://localhost:5000/api/Animes";
+
+  constructor(private http: HttpClient) {}
+
+  getAllData(): Observable<any> {
+    return this.http.get<any>(this.apiBaseUrl);
+  }
+
+  getCollectionsNames(): Observable<string[]> {
+    return this.http.get<string[]>(this.apiBaseUrl + "/collection-names");
+  }
+
+  getDataByCollection(collection: string): Observable<Anime[]> {
+    return this.http.get<Anime[]>(this.apiBaseUrl + "/" + collection);
+  }
+
+  getDataByCollectionAndName(collection: string, name: string): Observable<Anime> {
+    return this.http.get<Anime>(this.apiBaseUrl + "/" + collection + "/" + name);
+  }
+
+  getDataByTitleCoincidences(collection: string, input: string): Observable<Anime[]> {
+    return this.http.get<Anime[]>(this.apiBaseUrl + "/" + "anime-collection/search?searchbar=" + input);
+  }
+}
