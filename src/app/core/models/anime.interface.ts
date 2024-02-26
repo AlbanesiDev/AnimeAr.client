10aef8c66f922d4a35ce3497108a135bae919184
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-02-26 18:07:16 -0300

    feat: Interfaz provisional

---
+++ src/app/core/models/anime.interface.ts
@@ -0,0 +1,11 @@
+export interface Anime {
+  title:          string;
+  type:           string;
+  txtAlt:         string[];
+  genres:         string[];
+  synopsis:       string;
+  cover:          string;
+  status:         string;
+  related_animes: null;
+  episodes:       null;
+}
