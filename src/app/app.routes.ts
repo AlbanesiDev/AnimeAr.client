17b5dcce0366df336130986ad324bbdcf27eb875
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-02-28 18:08:32 -0300

    Feat: Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () => import("./public/pages/home/home.component").then((x) => x.HomeComponent),
+    title: "AnimeAr | Inicio",
+  },
+  {
+    path: "anime/:title",
+    loadComponent: () => import("./public/pages/anime/anime.component").then((x) => x.AnimeComponent),
+    title: (params: ActivatedRouteSnapshot) => {
+      const title = params.params["title"];
+      return `AnimeAr | ${title}`;
+    },
+  },
+  {
+    path: "directorio",
+    loadComponent: () => import("./public/pages/browse/browse.component").then((x) => x.BrowseComponent),
+    title: "AnimeAr | Directorio",
+  },
+  {
+    path: "register",
+    loadComponent: () => import("./public/pages/register/register.component").then((x) => x.RegisterComponent),
+    title: "AnimeAr | Registrarse",
+  },
+  {
+    path: "login",
+    loadComponent: () => import("./public/pages/login/login.component").then((x) => x.LoginComponent),
+    title: "AnimeAr | Iniciar sesión",
+  },
+];
