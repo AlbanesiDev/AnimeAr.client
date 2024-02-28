c305128915726e5b225f76e057ba5f23cb9c44fc
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-02-28 18:07:28 -0300

    feat: Firebase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import { provideClientHydration } from "@angular/platform-browser";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from "./app.routes";
+import { provideHttpClient, withFetch, withInterceptors } from "@angular/common/http";
+import { errorHandlerInterceptor } from "./core/interceptors/error-handler.interceptor";
+import { firebaseProviders } from "./firebase.config";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    firebaseProviders,
+    provideClientHydration(),
+    provideAnimations(),
+    provideHttpClient(withFetch(), withInterceptors([errorHandlerInterceptor])),
+  ],
 };
