f3d5c834b3cd4c1f638937220ad58167dbe2973c
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-02-28 18:03:11 -0300

    Fix: Route name change

--- src/app/core/services/anime-get.service.ts
+++ src/app/core/services/anime-get.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { Anime } from "../models/anime.interface";
+import { Anime } from "../interfaces/anime.interface";
 
 @Injectable({
   providedIn: "root",
