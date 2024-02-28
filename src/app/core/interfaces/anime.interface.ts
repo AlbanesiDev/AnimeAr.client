308f4bac4d1208cbfb44ff5610e668c869fdfcd5
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-02-28 18:01:54 -0300

    fix: Route name change

---
+++ src/app/core/interfaces/anime.interface.ts
@@ -0,0 +1,33 @@
+export interface Anime {
+  title: string;
+  type: Type;
+  txtAlt: string[];
+  genres: string[];
+  synopsis: string;
+  cover: string;
+  status: Status;
+  relatedAnimes: RelatedAnime[];
+  episodes: Episode[];
+}
+
+export type Type = "Anime" | "OVA" | "Película" | "Especial";
+
+export type Status = "Finalizado" | "En emisión" | "Próximamente";
+
+export interface RelatedAnime {
+  title: string;
+  relation: Relation;
+}
+
+export type Relation = "Precuela" | "Secuela" | "Historia principal" | "Historia paralela";
+
+export interface Episode {
+  title: string;
+  videos: VideoServer[];
+}
+
+export interface VideoServer {
+  server: string;
+  title: string;
+  url: string;
+}
