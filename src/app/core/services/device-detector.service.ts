7c5c3054064675fc13d02f16c46a3bc936847573
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-02-29 17:30:48 -0300

    feat: Device detector

---
+++ src/app/core/services/device-detector.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject, signal } from "@angular/core";
+import { DeviceDetectorService } from "ngx-device-detector";
+
+@Injectable({
+  providedIn: "root",
+})
+
+export class deviceDetectorService {
+  deviceDetectorService = inject(DeviceDetectorService);
+  isMobile = signal<boolean>(false);
+
+  constructor() {
+    this.mobileDetector();
+  }
+
+  mobileDetector() {
+    this.isMobile.set(this.deviceDetectorService.isMobile());
+  }
+}
