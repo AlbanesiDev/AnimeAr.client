d6cd6dc3b813c15e55ee61c224da3a8ccf9f05d4
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-02-29 17:36:53 -0300

    Fix: Creation of the dynamic forms group.

--- src/app/public/components/auth-form/auth-form.component.ts
+++ src/app/public/components/auth-form/auth-form.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from "@angular/common";
-import { HttpClient } from "@angular/common/http";
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, Output, inject } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, inject,
+} from "@angular/core";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { AuthService } from "../../../core/services/auth.service";
@@ -13,57 +13,60 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, FontAwesomeModule],
   template: `
-      <div class="form__wrapper">
-        <h1>{{ formUi[0].title }}</h1>
-        @if (errorMessage) {
-          <div class="error-message">{{ errorMessage }}</div>
-        }
-        <form class="form" [formGroup]="form" (submit)="submitEvent">
-          <div class="container">
-            <div class="form__group">
-              @for (item of formData; track $index) {
-                <input [type]="formData[0].type" [placeholder]="formData[0].placeholder" [formControlName]="formData[0].control" />
-              }
-              <button class="btn__submit" type="submit" [title]="formUi[0].buttonSubmit">{{ formUi[0].buttonSubmit }}</button>
-            </div>
-            @if(formUi[0].buttonForgot) {
-              <button>Olvidaste tu contraseña?</button>
+    <div class="form__wrapper">
+      <h1>{{ formUi[0].title }}</h1>
+      @if (errorMessage) {
+        <div class="error-message">{{ errorMessage }}</div>
+      }
+      <form class="form" [formGroup]="form">
+        <div class="container">
+          <div class="form__group">
+            @for (item of formData; track $index) {
+              <input
+                [type]="formData[$index].type"
+                [name]="formData[$index].name"
+                [placeholder]="formData[$index].placeholder"
+                [formControlName]="formData[$index].controlName"
+                [autocomplete]="formData[$index].autocomplete"
+              />
+
             }
-            <hr />
-            <div class="btn__group-vertical">
-              <button class="btn__social-form">
-                <img src="/assets/icons/facebook.svg" alt="">
-                <div>
-                  <span>
-                    Facebook
-                  </span>
-                </div>
-              </button>
-              <button class="btn__social-form">
-                <img src="/assets/icons/google.svg" alt="">
-                <div>
-                  <span>
-                    Google
-                  </span>
-                </div>
-              </button>
-              <button class="btn__social-form">
-                <img src="/assets/icons/apple.svg" alt="">
-                <div>
-                  <span>
-                    Apple
-                  </span>
-                </div>
-              </button>
-            </div>
+            <button class="btn__submit" type="submit" [title]="formUi[0].buttonSubmit" (click)="onSubmit()">
+              {{ formUi[0].buttonSubmit }}
+            </button>
           </div>
-        </form>
-      </div>
+          @if (formUi[0].buttonForgot) {
+            <button>Olvidaste tu contraseña?</button>
+          }
+          <hr />
+          <div class="btn__group-vertical">
+            <button class="btn__social-form">
+              <img src="/assets/icons/facebook.svg" alt="" />
+              <div>
+                <span> Facebook </span>
+              </div>
+            </button>
+            <button class="btn__social-form" (click)="signInWithGoogle()">
+              <img src="/assets/icons/google.svg" alt="" />
+              <div>
+                <span> Google </span>
+              </div>
+            </button>
+            <button class="btn__social-form">
+              <img src="/assets/icons/apple.svg" alt="" />
+              <div>
+                <span> Apple </span>
+              </div>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   `,
   styleUrl: "./auth-form.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthFormComponent {
+export class AuthFormComponent implements OnInit {
   // Dynamic Form
   @Input() public errorMessage: string | null = null;
   @Input() public formUi!: FormUiInterface[];
@@ -71,17 +74,30 @@ export class AuthFormComponent {
   @Output() public submitEvent = new EventEmitter();
   // Icons
   public faGoogle = faGoogle;
-
   // Injects
   public fb = inject(FormBuilder);
-  public http = inject(HttpClient);
   public authService = inject(AuthService);
   public router = inject(Router);
   public cdr = inject(ChangeDetectorRef);
+  // Variables
+  public form = this.fb.group({});
 
-  form = this.fb.nonNullable.group({
-    username: ["", Validators.required, Validators.minLength(3), Validators.maxLength(20)],
-    email: ["", Validators.required, Validators.email],
-    password: ["", Validators.required, Validators.minLength(8), Validators.maxLength(30)],
-  });
+  ngOnInit() {
+    const formControls: {[key: string]: any} = {};
+    this.formData.forEach((field) => {
+      formControls[field.controlName] = ["", field.validators];
+    });
+    this.form = this.fb.group(formControls);
+    this.cdr.detectChanges();
+  }
+
+  onSubmit(){
+    if(this.form.valid){
+      this.submitEvent.emit(this.form.value);
+    }
+  }
+
+  signInWithGoogle() {
+    this.authService.signInWithGoogle();
+  }
 }
