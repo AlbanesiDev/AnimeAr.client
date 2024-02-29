d5bc7813092def85bfa7acf8b4933f428fc765ed
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-02-29 17:41:49 -0300

    feat: New routes and canActivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { ActivatedRouteSnapshot, Routes } from "@angular/router";
+import { authGuard } from "./core/guards/auth.guard";
 
 export const routes: Routes = [
   {
@@ -20,12 +21,26 @@ export const routes: Routes = [
     title: "AnimeAr | Directorio",
   },
   {
+    path: "mis-animes",
+    canActivate: [authGuard],
+    loadComponent: () => import("./public/pages/my-animes/my-animes.component").then((x) => x.MyAnimesComponent),
+    title: "AnimeAr | Mis Animes",
+  },
+  {
+    path: "perfil",
+    canActivate: [authGuard],
+    loadComponent: () => import("./public/pages/profile/profile.component").then((x) => x.ProfileComponent),
+    title: "AnimeAr | Perfil",
+  },
+  {
     path: "register",
+    canActivate: [() => !authGuard],
     loadComponent: () => import("./public/pages/register/register.component").then((x) => x.RegisterComponent),
     title: "AnimeAr | Registrarse",
   },
   {
     path: "login",
+    canActivate: [() => !authGuard],
     loadComponent: () => import("./public/pages/login/login.component").then((x) => x.LoginComponent),
     title: "AnimeAr | Iniciar sesión",
   },
